0177d805e9c403b03706ac3987e37c8f363919d7
Pakitof <127530418+pakitof@users.noreply.github.com>
2024-05-18 06:17:41 +0200

    Agregando sección navbar, agregando logo, modificando estilo menú y corrigiendo altura de la página

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,8 @@
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// Importar Bootstrap JS
+import "bootstrap";
+
+// Importar Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importar Bootstrap Icons CSS
+import "bootstrap-icons/font/bootstrap-icons.css";
