c60c8bf202bdea31dc585873f50bd57387a61abd
Pakitof <127530418+pakitof@users.noreply.github.com>
2024-05-18 21:10:54 +0200

    Creando sección de galería interactiva con Bootstrap5

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,3 +1,6 @@
+// Importa jQuery
+import $ from "jquery";
+
 // Importar Bootstrap JS
 import "bootstrap";
 
@@ -6,3 +9,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // Importar Bootstrap Icons CSS
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+// Importa MDB UI Kit
+import "mdb-ui-kit/css/mdb.min.css";
+import "mdb-ui-kit/js/mdb.es.min.js";
+
+import "lightbox2/dist/css/lightbox.min.css";
+import "lightbox2/dist/js/lightbox.min.js";
